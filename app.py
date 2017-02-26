24d58ea55abee6aa06b51d51dd9621ff27148268
srini <srinivasarajui@gmail.com>
2017-02-26 09:58:35 +0000

    commit1
    git commit -m commit1

---
+++ app.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import urllib
+import json
+import os
+import urllib2
+
+import duckduckgo
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeWebhookResult(req):
+    if req.get("result").get("action") != "whatis":
+        return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    key = parameters.get("given-name")
+    print(key)
+    speech = duckduckgo.query(key).abstract.text;
+    if speech=="":
+        speech = duckduckgo.query(key).related[0].text;
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        #"data": {},
+        # "contextOut": [],
+        "source": "zeva-saerch"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+    app.run(debug=True, port=port, host='0.0.0.0')
